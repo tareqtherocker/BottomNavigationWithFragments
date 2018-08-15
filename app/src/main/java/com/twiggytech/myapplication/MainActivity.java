e5ddb59bc396f7b5bfbc4cfe543d3a4a25843327
tareq <tareqtherocker@gmail.com>
2018-08-15 18:22:15 +0600

    bottom listener initialized

--- app/src/main/java/com/twiggytech/myapplication/MainActivity.java
+++ app/src/main/java/com/twiggytech/myapplication/MainActivity.java
@@ -1,15 +1,50 @@
 package com.twiggytech.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a;
+        bottomNavigationView = findViewById(R.id.id_bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.id_menu_home:
+                break;
+
+            case R.id.id_menu_blood_list:
+                break;
+
+            case R.id.id_menu_messenger:
+                break;
+
+            case R.id.id_menu_friends:
+                break;
+
+            case R.id.id_menu_notifications:
+                break;
+
+
+        }
+
+
+        return false;
     }
 }
